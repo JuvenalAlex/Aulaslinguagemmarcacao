b6cf0d6b219871a168b95cf6a56c3e4da688af27
JuvenalAlex <61656798+JuvenalAlex@users.noreply.github.com>
2020-03-09 16:04:09 -0300

    Add files via upload

---
+++ 1059.py
@@ -0,0 +1,5 @@
+x = 100
+x = int(input(''))
+for i in range(1, 100+1):
+    if i % 2 == 0:
+        print('{}'.format(i))
---
+++ 1151.py
@@ -0,0 +1,6 @@
+N = int(input())
+numero=0
+for i in range(1, 5):
+    numero=numero+1
+    print(numero)
+ 
